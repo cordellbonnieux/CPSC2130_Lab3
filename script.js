f2e5a1dc091ce9e8e14fb28e35191037f5c7d8a2
Cordell <cordellbonnieux@gmail.com>
2022-02-04 13:53:39 -0800

    Added player sprite and created basic movement script.

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const canvas = document.getElementsByTagName('canvas')[0];
 const ctx = canvas.getContext('2d');
+const TILE_SIZE = 32;
 
 /*
 *   CLASSES
 */
 class Unit {
-    constructor(name, hp, speed, size, x, y, sprite) {
+    constructor(name, hp, speed, x, y, sprite) {
         this.name = name;
         this.hp = hp;
         this.speed = speed;
-        this.size = size;
+        this.size = TILE_SIZE;
         this.x = x;
         this.y = y;
         this.sprite = new Image();
         this.sprite.src = sprite;
-        this.sprite.width = size;
-        this.sprite.height = size;
+        this.sprite.width = TILE_SIZE;
+        this.sprite.height = TILE_SIZE;
     }
     render() {
         ctx.drawImage(this.sprite, this.x, this.y);
@@ -23,30 +24,60 @@ class Unit {
     moveTo (x, y) {
         this.x = x;
         this.y = y;
-        // add animate logic
-    }
-    animate() {
-
     }
 }
 
 class Enemy extends Unit {
-    constructor(name, hp, speed, size, x, y, sprite) {
-        super(name, hp, speed, size, x, y, sprite);
+    constructor(name, hp, speed, x, y, sprite) {
+        super(name, hp, speed, x, y, sprite);
     }
 }
 
 class Player extends Unit {
-    constructor(name, hp, speed, size, x, y) {
-        super(name, hp, speed, size, x, y, './sprites/test.png');
+    constructor(name, hp, speed, x, y) {
+        super(name, hp, speed, x, y, './sprites/player.png');
+    }
+    addControls() {
+        window.addEventListener('keydown', (e) => {
+            if (e.key == 'a' || e.key == 'ArrowLeft') {
+                this.x -= 1 * this.speed;
+            } else if (e.key == 'd' || e.key == 'ArrowRight') {
+                this.x += 1 * this.speed;
+            } else if (e.key == 'w' || e.key == 'ArrowUp') {
+                this.y -= 1 * this.speed;
+            } else if (e.key == 's' || e.key == 'ArrowDown') {
+                this.y += 1 * this.speed;
+            }
+        })
     }
 }
 
+class Crosshair {
+    constructor() {
+        this.img = new Image();
+        this.img.src = './sprites/crosshairs.png';
+        this.img.width = TILE_SIZE;
+        this.img.height = TILE_SIZE;
+        this.x;
+        this.y;
+    }
+    addToContext() {
+        canvas.addEventListener('mousemove', (e) => {
+            this.x = e.clientX;
+            this.y = e.clientY;
+        })
+    }
+    render() {
+        ctx.drawImage(this.img, this.x, this.y);
+    }
+}
 /*
 *   INSTANTIATE UNITS
 */
 
-let test = new Player('player', 100, 5, 32, 100, 150);
+const player = new Player('player', 100, 25, 100, 150);
+const crosshairs = new Crosshair();
+
 
 /**
  *  ENGINE FUNCTIONS
@@ -60,9 +91,14 @@ function main() {
 
 // Called on start
 function startGame() {
+    // preload assets
+    crosshairs.addToContext();
+    player.addControls();
+
+    // start
     updateGame();
     window.requestAnimationFrame(drawGame);
-    crosshairs();
+
 }
 
 // Called Every
@@ -70,7 +106,7 @@ function updateGame() {
     setCanvasDimensions(canvas); // ensures canvas dimensions == viewport
 
     // GAME AND ANIMATION LOGIC GOES HERE
-    test.moveTo(300, 400);
+
 
     // CHANGE THE NUMBER OF MILLISECONDS TO ADJUST FRAME RATE
     window.setTimeout(updateGame, 20);
@@ -80,7 +116,8 @@ function drawGame() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
 
     // RENDERING HAPPENS HERE
-    test.render();
+    player.render();
+    crosshairs.render();
     
 
     window.requestAnimationFrame(drawGame);
@@ -95,16 +132,5 @@ function setCanvasDimensions(c) {
     c.width = window.innerWidth;
     c.height = window.innerHeight;
     // for now
-    canvas.style.backgroundColor = 'blue';
-}
-
-function crosshairs() {
-    canvas.addEventListener('mousemove', (e) => {
-        console.log(e.clientX)
-        console.log("hey")
-        // replace with image!
-        ctx.beginPath();
-        ctx.arc(e.clientX, e.clientY, 30, 0, 2 * Math.PI, false);
-        ctx.stroke();
-    })
+    canvas.style.backgroundColor = '#000';
 }
